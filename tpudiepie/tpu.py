a5dd1bd49e2292bf35ff3946310307ecb564b54c
Shawn Presser <shawnpresser@gmail.com>
2020-06-08 22:00:45 -0700

    Fix python3.6; version 0.0.4

--- tpudiepie/tpu.py
+++ tpudiepie/tpu.py
@@ -87,6 +87,24 @@ def get_tpu_zones():
     #"us-east1-d",
   ]
 
+class nullcontext(contextlib.AbstractContextManager):
+    """Context manager that does no additional processing.
+    Used as a stand-in for a normal context manager, when a particular
+    block of code is only sometimes used with a normal context manager:
+    cm = optional_cm if condition else nullcontext()
+    with cm:
+        # Perform operation, using optional_cm if condition is True
+    """
+
+    def __init__(self, enter_result=None):
+        self.enter_result = enter_result
+
+    def __enter__(self):
+        return self.enter_result
+
+    def __exit__(self, *excinfo):
+        pass
+
 def click_context():
   try:
     import click
@@ -94,7 +112,7 @@ def click_context():
   except:
     ctx = None
   if ctx is None:
-    ctx = contextlib.nullcontext()
+    ctx = nullcontext()
   return ctx
 
 @ring.lru(expire=15) # cache tpu info for 15 seconds
